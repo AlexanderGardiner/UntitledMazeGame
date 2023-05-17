169df19edd152ca7c53da6ecec5a645434acf2ab
AlexanderGardiner <misc@alexandergardiner.com>
2023-05-17 14:28:44 -0700

    its still too fast

--- public/SceneClasses/EventLoop.mjs
+++ public/SceneClasses/EventLoop.mjs
@@ -20,16 +20,46 @@ export default class EventLoop {
             }
             
         } 
-        console.log(player.getSpeed());
+        
+        if (this.keyboardInput.getDownPressed()) {
+            if (player.getSpeed+playerMovementParameters.getAcceleration()>=-player.getMovementParameters().getMaxSpeed()) {
+                player.setSpeed(player.getSpeed().addVector3D(new Vector3D(0,player.getSpeed()-playerMovementParameters.getAcceleration(),0)));
+            } else {
+                player.setSpeed(player.getSpeed().addVector3D(new Vector3D(0,-playerMovementParameters.getMaxSpeed(),0)));
+            }
+            
+        } 
+
+    
     
 
 
 
         player.setPosition3D(player.getPosition3D().addVector3D(player.getSpeed()));
         let xSpeedAdjustment = 0;
-        if (player.getSpeed().getXMagnitude>0) {
-            
-        }
+        let ySpeedAdjustment = 0;
+        let zSpeedAdjustment = 0;
+
+        if (player.getSpeed().getXMagnitude<0) {
+            xSpeedAdjustment = playerMovementParameters.getDeceleration();
+        } else if (player.getSpeed().getXMagnitude>0) {
+            xSpeedAdjustment = -playerMovementParameters.getDeceleration()
+        } 
+
+        if (player.getSpeed().getYMagnitude<0) {
+            ySpeedAdjustment = playerMovementParameters.getDeceleration();
+        } else if (player.getSpeed().getYMagnitude>0) {
+            ySpeedAdjustment = -playerMovementParameters.getDeceleration()
+        } 
+
+        if (player.getSpeed().getZMagnitude<0) {
+            zSpeedAdjustment = playerMovementParameters.getDeceleration();
+        } else if (player.getSpeed().getZMagnitude>0) {
+            zSpeedAdjustment = -playerMovementParameters.getDeceleration()
+        } 
+
+        player.setSpeed(player.getSpeed().addVector3D(new Vector3D(xSpeedAdjustment, ySpeedAdjustment, zSpeedAdjustment)));
+
         this.scene.clearScene();
         this.scene.updateScene();
     }
