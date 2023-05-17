922689d798a9e6344cceeb1dfcab879d2933fb32
AlexanderGardiner <misc@alexandergardiner.com>
2023-05-17 14:23:27 -0700

    It moves now but with no deceleration

--- public/SceneClasses/EventLoop.mjs
+++ public/SceneClasses/EventLoop.mjs
@@ -9,45 +9,27 @@ export default class EventLoop {
     }
 
     update() {
-        let additionalPlayerSpeed = new Vector3D(0, 0, 0)
         let player = this.scene.getPlayer();
+        let playerMovementParameters = player.getMovementParameters()
 
         if (this.keyboardInput.getUpPressed()) {
-            if (player.getSpeed<player.getMovementParameters().getMaxSpeed()) {
-                player.setSpeed(player.getSpeed+player.getMovementParameters().getAcceleration());
+            if (player.getSpeed+playerMovementParameters.getAcceleration()<=player.getMovementParameters().getMaxSpeed()) {
+                player.setSpeed(player.getSpeed().addVector3D(new Vector3D(0,player.getSpeed()+playerMovementParameters.getAcceleration(),0)));
+            } else {
+                player.setSpeed(player.getSpeed().addVector3D(new Vector3D(0,playerMovementParameters.getMaxSpeed(),0)));
             }
             
+        } 
+        console.log(player.getSpeed());
+    
+
+
+
+        player.setPosition3D(player.getPosition3D().addVector3D(player.getSpeed()));
+        let xSpeedAdjustment = 0;
+        if (player.getSpeed().getXMagnitude>0) {
+            
         }
-
-        if (this.keyboardInput.getDownPressed()) {
-            if (player.getSpeed().getYMagnitude()-5>=player.getMaxMovementSpeed()) {
-                additionalPlayerSpeed.setYMagnitude(additionalPlayerSpeed.getXMagnitude() - 5);
-            } else {
-                additionalPlayerSpeed.setYMagnitude(-player.getMaxMovementSpeed() - player.getSpeed());
-            }
-        }
-
-
-
-        if (this.keyboardInput.getRightPressed()) {
-            if (player.getSpeed().getXMagnitude()+5<=player.getMaxMovementSpeed()) {
-                additionalPlayerSpeed.setXMagnitude(additionalPlayerSpeed.getXMagnitude() + 5);
-            } else {
-                additionalPlayerSpeed.setXMagnitude(player.getMaxMovementSpeed() - player.getSpeed());
-            }
-        }
-
-        if (this.keyboardInput.getLeftPressed()) {
-            if (player.getSpeed().getXMagnitude()-5>=player.getMaxMovementSpeed()) {
-                additionalPlayerSpeed.setXMagnitude(additionalPlayerSpeed.getXMagnitude() - 5);
-            } else {
-                additionalPlayerSpeed.setXMagnitude(-player.getMaxMovementSpeed() - player.getSpeed());
-            }
-        }
-
-        player.setSpeed(player.getSpeed().addVector3D(additionalPlayerSpeed));
-
-        player.setPosition3D(player.getPosition3D().addVector3D(this.scene.getPlayer().getSpeed()));
         this.scene.clearScene();
         this.scene.updateScene();
     }
